f1c496fc10f3b9f5bb488af2539a96ccb9355532
Choi Gunho <ghc0311@naver.com>
2017-03-29 00:49:38 +0900

    Add files via upload

---
+++ image_reader.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+batch_size =16
+epoch = 10 
+# Read image files
+
+shoes_filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once("./shoes/*.jpg"),num_epochs=epoch)
+bags_filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once("./bags/*.jpg"),num_epochs=epoch)
+image_reader = tf.WholeFileReader()
+
+_, shoes_file = image_reader.read(shoes_filename_queue)
+_, bags_file = image_reader.read(bags_filename_queue)
+
+shoes_image = tf.image.decode_jpeg(shoes_file)
+bags_image = tf.image.decode_jpeg(bags_file)
+
+shoes_image = tf.cast(tf.reshape(shoes_image,shape=[64,64,3]),dtype=tf.float32)
+bags_image = tf.cast(tf.reshape(bags_image,shape=[64,64,3]),dtype=tf.float32)
+
+num_preprocess_threads = 1
+min_queue_examples = 256
+batch_shoes = tf.train.shuffle_batch([shoes_image],
+									    batch_size=batch_size,
+									    num_threads=num_preprocess_threads,
+									    capacity=min_queue_examples + 3 * batch_size,
+									    min_after_dequeue=min_queue_examples)
+
+batch_bags = tf.train.shuffle_batch([bags_image],
+									    batch_size=batch_size,
+									    num_threads=num_preprocess_threads,
+									    capacity=min_queue_examples + 3 * batch_size,
+									    min_after_dequeue=min_queue_examples)
+
+
+
+
+with tf.Session() as sess:
+	for i in range(epoch):
+		print(batch_shoes,batch_bags)
